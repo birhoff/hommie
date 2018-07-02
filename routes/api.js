11e511d87669c55d30dcbf8ae935fce88e478229
Konstantin Gladkikh <birhoff@gmail.com>
2018-07-02 17:27:49 +0300

    feat(api): youla

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const youla = require('./youla');
+
 /* GET users listing. */
 // Put all API endpoints under '/api'
 router.get('/passwords', (req, res) => {
     // Return them as json
     res.json({ status: "OK" });
 });
+
+router.get('/youla', youla);
 
 module.exports = router;
