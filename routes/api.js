1c16dbe553b0fd198bafbc295c4bfe9506c4cc07
Konstantin Gladkikh <birhoff@gmail.com>
2018-07-02 18:36:35 +0300

    feat(api): enable cors

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,12 @@ const youla = require('./youla');
 
 /* GET users listing. */
 // Put all API endpoints under '/api'
+router.get('/', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 router.get('/passwords', (req, res) => {
     // Return them as json
     res.json({ status: "OK" });
