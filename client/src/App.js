4977e83003299d9b5f30de44316e5b101db978ae
Konstantin Gladkikh <birhoff@gmail.com>
2018-09-03 15:47:13 +0300

    fix(metrika): Fix metrika init

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,24 @@
 import React, { Component } from 'react';
+import { YMInitializer } from 'react-yandex-metrika';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <!-- Yandex.Metrika counter -->
-          <script type="text/javascript" >
-              (function (d, w, c) {
-              (w[c] = w[c] || []).push(function() {
-                  try {
-                      w.yaCounter50198683 = new Ya.Metrika2({
-                          id:50198683,
-                          clickmap:true,
-                          trackLinks:true,
-                          accurateTrackBounce:true
-                      });
-                  } catch(e) { }
-              });
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to My Home</h1>
+                </header>
+                <p className="App-intro">
+                    Hello this is my Home
+                </p>
+                <YMInitializer accounts={[50198683]}/>
+            </div>
 
-              var n = d.getElementsByTagName("script")[0],
-              s = d.createElement("script"),
-              f = function () { n.parentNode.insertBefore(s, n); };
-              s.type = "text/javascript";
-              s.async = true;
-              s.src = "https://mc.yandex.ru/metrika/tag.js";
-
-              if (w.opera == "[object Opera]") {
-              d.addEventListener("DOMContentLoaded", f, false);
-          } else { f(); }
-          })(document, window, "yandex_metrika_callbacks2");
-          </script>
-          <noscript><div><img src="https://mc.yandex.ru/watch/50198683" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-          <!-- /Yandex.Metrika counter -->
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
