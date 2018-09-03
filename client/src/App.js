a550882315737942fdef50c4485caf4e60a82f97
Konstantin Gladkikh <birhoff@gmail.com>
2018-09-03 15:36:50 +0300

    feat(metrika): Add metrika

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,34 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <!-- Yandex.Metrika counter -->
+          <script type="text/javascript" >
+              (function (d, w, c) {
+              (w[c] = w[c] || []).push(function() {
+                  try {
+                      w.yaCounter50198683 = new Ya.Metrika2({
+                          id:50198683,
+                          clickmap:true,
+                          trackLinks:true,
+                          accurateTrackBounce:true
+                      });
+                  } catch(e) { }
+              });
+
+              var n = d.getElementsByTagName("script")[0],
+              s = d.createElement("script"),
+              f = function () { n.parentNode.insertBefore(s, n); };
+              s.type = "text/javascript";
+              s.async = true;
+              s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+              if (w.opera == "[object Opera]") {
+              d.addEventListener("DOMContentLoaded", f, false);
+          } else { f(); }
+          })(document, window, "yandex_metrika_callbacks2");
+          </script>
+          <noscript><div><img src="https://mc.yandex.ru/watch/50198683" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+          <!-- /Yandex.Metrika counter -->
       </div>
     );
   }
